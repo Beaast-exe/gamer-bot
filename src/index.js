83d521762c9a0a0ffae489e76b7ecbc4ec5cb455
Beaast-exe <r.fariaa18@gmail.com>
2025-01-20 11:56:16 +0000

    added error logging to console

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ client.distube = new DisTube(client, {
 			cookies: [
 				{
 					'domain': '.youtube.com',
-					'expirationDate': 1766164960.94132,
+					'expirationDate': 1771800595.236496,
 					'hostOnly': false,
 					'httpOnly': false,
 					'name': '__Secure-1PAPISID',
@@ -42,12 +42,12 @@ client.distube = new DisTube(client, {
 					'secure': true,
 					'session': false,
 					'storeId': '0',
-					'value': '9cX380Y6fdbFGjSB/ASFkA0nCQHunCGi8j',
+					'value': 'ZHqEYCinyIbuXkMg/AUOSyNeiDinwxpeA6',
 					'id': 1
 				},
 				{
 					'domain': '.youtube.com',
-					'expirationDate': 1766164960.941243,
+					'expirationDate': 1771800595.236541,
 					'hostOnly': false,
 					'httpOnly': true,
 					'name': '__Secure-1PSID',
@@ -56,12 +56,12 @@ client.distube = new DisTube(client, {
 					'secure': true,
 					'session': false,
 					'storeId': '0',
-					'value': 'g.a000qQh48bRhje_W9PTW5VRNgo-pZgBub2UnY02I8mUqLTczdcSpDQ_g-1OhZXv18lNZJeJVxgACgYKAeASARESFQHGX2MidlXxq1fSPhFHiJJ2TKW-cxoVAUF8yKo84LC86ckFlSt1lyCAFXID0076',
+					'value': 'g.a000sgh48V28BPZv_rIQ465uLDZiSiCFURKSEhxpOHr6-EtfIKPns3_RZbV4ZFDuexAHgE4EiAACgYKAUkSARESFQHGX2Mi72guZ-zY0sgtMBHWRjOXNxoVAUF8yKqz7xV58bLDTHO-4j7up2tj0076',
 					'id': 2
 				},
 				{
 					'domain': '.youtube.com',
-					'expirationDate': 1763978478.468244,
+					'expirationDate': 1768908443.850007,
 					'hostOnly': false,
 					'httpOnly': true,
 					'name': '__Secure-1PSIDCC',
@@ -70,12 +70,12 @@ client.distube = new DisTube(client, {
 					'secure': true,
 					'session': false,
 					'storeId': '0',
-					'value': 'AKEyXzUGKZ2bvdbLyl3OtMDQXXMT3Nb4d0mwrACbHq_Kr5mPUo9Lh4iKp0079zI5-jfV0hC-zA',
+					'value': 'AKEyXzXrRQBG-i2-IlZqZlIkGyX1ZVXDJXfOP7DEktZgKR0lF4CVQIpR5L8U4yWmzB5wJxGx93Q',
 					'id': 3
 				},
 				{
 					'domain': '.youtube.com',
-					'expirationDate': 1763978202.447009,
+					'expirationDate': 1768908205.912985,
 					'hostOnly': false,
 					'httpOnly': true,
 					'name': '__Secure-1PSIDTS',
@@ -84,12 +84,12 @@ client.distube = new DisTube(client, {
 					'secure': true,
 					'session': false,
 					'storeId': '0',
-					'value': 'sidts-CjEBQT4rX1UWDVTOP6R56gfNFT9f7VuyAm2MY-YnTikPV_GSEWLltqiedtoFWdKQypp9EAA',
+					'value': 'sidts-CjEBmiPuTbwG_qHd4B27y2Y-T7JXerSX_wXaQBuRCXypcoS2YaeBxtxn8KkPHYIK-6QGEAA',
 					'id': 4
 				},
 				{
 					'domain': '.youtube.com',
-					'expirationDate': 1766164960.941332,
+					'expirationDate': 1771800595.236511,
 					'hostOnly': false,
 					'httpOnly': false,
 					'name': '__Secure-3PAPISID',
@@ -98,12 +98,12 @@ client.distube = new DisTube(client, {
 					'secure': true,
 					'session': false,
 					'storeId': '0',
-					'value': '9cX380Y6fdbFGjSB/ASFkA0nCQHunCGi8j',
+					'value': 'ZHqEYCinyIbuXkMg/AUOSyNeiDinwxpeA6',
 					'id': 5
 				},
 				{
 					'domain': '.youtube.com',
-					'expirationDate': 1766164960.941257,
+					'expirationDate': 1771800595.236557,
 					'hostOnly': false,
 					'httpOnly': true,
 					'name': '__Secure-3PSID',
@@ -112,12 +112,12 @@ client.distube = new DisTube(client, {
 					'secure': true,
 					'session': false,
 					'storeId': '0',
-					'value': 'g.a000qQh48bRhje_W9PTW5VRNgo-pZgBub2UnY02I8mUqLTczdcSpcs1VnsXFfRogS9Y_QFeiiAACgYKAQkSARESFQHGX2MiQ7P52uCys26IPOLIScgHnhoVAUF8yKruJFziRXcNVrNoTtD4bPOL0076',
+					'value': 'g.a000sgh48V28BPZv_rIQ465uLDZiSiCFURKSEhxpOHr6-EtfIKPn8fPTCnXGRm8rCSkGozo2tQACgYKAQsSARESFQHGX2MimUuD5UNW5lR5Ra_b01tIIxoVAUF8yKozuXUXDzUVXKFYEJ3dZG-x0076',
 					'id': 6
 				},
 				{
 					'domain': '.youtube.com',
-					'expirationDate': 1763978478.46828,
+					'expirationDate': 1768908443.850047,
 					'hostOnly': false,
 					'httpOnly': true,
 					'name': '__Secure-3PSIDCC',
@@ -126,12 +126,12 @@ client.distube = new DisTube(client, {
 					'secure': true,
 					'session': false,
 					'storeId': '0',
-					'value': 'AKEyXzU_cXwom2gfWNKi_jZJ_hPsKNO9Nj7v5IJ8lT6bQh_Z0fa3HRcaoMmMPmYa2G9CnMyMDtI',
+					'value': 'AKEyXzV6OByJXTKQqUIvAv-CDWu-g3SoZKW21Pfc95XXHstP-FRbkzws8yGHfnCOY_FoGhWo9_S_',
 					'id': 7
 				},
 				{
 					'domain': '.youtube.com',
-					'expirationDate': 1763978202.44708,
+					'expirationDate': 1768908205.913062,
 					'hostOnly': false,
 					'httpOnly': true,
 					'name': '__Secure-3PSIDTS',
@@ -140,12 +140,12 @@ client.distube = new DisTube(client, {
 					'secure': true,
 					'session': false,
 					'storeId': '0',
-					'value': 'sidts-CjEBQT4rX1UWDVTOP6R56gfNFT9f7VuyAm2MY-YnTikPV_GSEWLltqiedtoFWdKQypp9EAA',
+					'value': 'sidts-CjEBmiPuTbwG_qHd4B27y2Y-T7JXerSX_wXaQBuRCXypcoS2YaeBxtxn8KkPHYIK-6QGEAA',
 					'id': 8
 				},
 				{
 					'domain': '.youtube.com',
-					'expirationDate': 1765732118.433525,
+					'expirationDate': 1765732118.213437,
 					'hostOnly': false,
 					'httpOnly': true,
 					'name': '__Secure-YEC',
@@ -154,12 +154,12 @@ client.distube = new DisTube(client, {
 					'secure': true,
 					'session': false,
 					'storeId': '0',
-					'value': 'CgtMQ05RNmhLanh6SSiv2ou6BjIiCgJQVBIcEhgSFhMLFBUWFwwYGRobHB0eHw4PIBAREiEgJw%3D%3D',
+					'value': 'CgtMQ05RNmhLanh6SSimtbi8BjIiCgJQVBIcEhgSFhMLFBUWFwwYGRobHB0eHw4PIBAREiEgJw%3D%3D',
 					'id': 9
 				},
 				{
 					'domain': '.youtube.com',
-					'expirationDate': 1766164960.941295,
+					'expirationDate': 1771800595.236466,
 					'hostOnly': false,
 					'httpOnly': false,
 					'name': 'APISID',
@@ -168,12 +168,12 @@ client.distube = new DisTube(client, {
 					'secure': false,
 					'session': false,
 					'storeId': '0',
-					'value': 'DuxQLa3Qmz5HcVka/Ab7Feom-0oBxubNSP',
+					'value': 'DSP6MnRmshP8dINT/AXsgY7YhjeCNhpDRl',
 					'id': 10
 				},
 				{
 					'domain': '.youtube.com',
-					'expirationDate': 1766164960.941272,
+					'expirationDate': 1771800595.236433,
 					'hostOnly': false,
 					'httpOnly': true,
 					'name': 'HSID',
@@ -182,12 +182,12 @@ client.distube = new DisTube(client, {
 					'secure': false,
 					'session': false,
 					'storeId': '0',
-					'value': 'AzNKUpO6Y8pR8Tb4P',
+					'value': 'A8QAxRXWsmBp5f1B9',
 					'id': 11
 				},
 				{
 					'domain': '.youtube.com',
-					'expirationDate': 1767002472.844039,
+					'expirationDate': 1767002566.899352,
 					'hostOnly': false,
 					'httpOnly': true,
 					'name': 'LOGIN_INFO',
@@ -196,12 +196,12 @@ client.distube = new DisTube(client, {
 					'secure': true,
 					'session': false,
 					'storeId': '0',
-					'value': 'AFmmF2swRQIgPzHiN56q8eTcBAX-f4l0vEeMwBQ_iL14EqbA2iL-i6UCIQDkckN6fW07uATxg97QWl2DWLYjKRZ1JDLktBjJ9_VUHw:QUQ3MjNmeENMTUVVaFV6U2hkY09NdEdLSWJjM2pPanA4endjbFQ5SUhoTGRTT2NtTmJTZWFZVlRDRmJzalhRQVpOSlJKTTVrSFdPdGFjb21lSlRsR19rZFJsV0hjWGhYbVhwV01sSnlEbENRR013UUhkNkpVa0J5Vy1iUlEyQ3RNQTJIRTYxcENwdFdhM1ZDbDROcFJzejExZzVvREVxRFZ3',
+					'value': 'AFmmF2swRAIgD5jJTXMljsgV85hG5JTsZs6tPpaRK7C11-hs5W7K89ICIFCFvGAl1RpyXflimmTKQReDdFGPVMx8ezeQmWbKuGtn:QUQ3MjNmeXVKeTI2MnROQ1Nxb2NaSlNzSjNKVXV4U1NvTnJLZklWdlBjZVExVnpTam5wb01jbWszQUg3cVJWZUZWQUd0Qm1uNzhDRVZXTnBRRDBhZ2JPUmltYlNZenAwR1pvSXlQWENqZ2gwbkRROWJGVTRuY2pmNnFPeEdpV3ZkbVVMalNqYVJqV3g1UjRta21VUnBjN0dFcXBSTkJaVGxR',
 					'id': 12
 				},
 				{
 					'domain': '.youtube.com',
-					'expirationDate': 1767002475.723126,
+					'expirationDate': 1771932443.214879,
 					'hostOnly': false,
 					'httpOnly': false,
 					'name': 'PREF',
@@ -215,7 +215,7 @@ client.distube = new DisTube(client, {
 				},
 				{
 					'domain': '.youtube.com',
-					'expirationDate': 1766164960.941307,
+					'expirationDate': 1771800595.236482,
 					'hostOnly': false,
 					'httpOnly': false,
 					'name': 'SAPISID',
@@ -224,12 +224,12 @@ client.distube = new DisTube(client, {
 					'secure': true,
 					'session': false,
 					'storeId': '0',
-					'value': '9cX380Y6fdbFGjSB/ASFkA0nCQHunCGi8j',
+					'value': 'ZHqEYCinyIbuXkMg/AUOSyNeiDinwxpeA6',
 					'id': 14
 				},
 				{
 					'domain': '.youtube.com',
-					'expirationDate': 1766164960.941152,
+					'expirationDate': 1771800595.236525,
 					'hostOnly': false,
 					'httpOnly': false,
 					'name': 'SID',
@@ -238,12 +238,12 @@ client.distube = new DisTube(client, {
 					'secure': false,
 					'session': false,
 					'storeId': '0',
-					'value': 'g.a000qQh48bRhje_W9PTW5VRNgo-pZgBub2UnY02I8mUqLTczdcSp2K77vbSdcO77b4ZvocfElwACgYKATgSARESFQHGX2MixR1h9BPFij3N_3h4QfGskxoVAUF8yKpd1563f-DPVl30Tb9CMbC80076',
+					'value': 'g.a000sgh48V28BPZv_rIQ465uLDZiSiCFURKSEhxpOHr6-EtfIKPn9FRucz49-3w0aEPwjya0rwACgYKAaoSARESFQHGX2MiVYagK_PdkXclYk4fXsHaAxoVAUF8yKo1ibwyyvDCRoDTHZ2tvvng0076',
 					'id': 15
 				},
 				{
 					'domain': '.youtube.com',
-					'expirationDate': 1763978478.468217,
+					'expirationDate': 1768908443.849918,
 					'hostOnly': false,
 					'httpOnly': false,
 					'name': 'SIDCC',
@@ -252,12 +252,12 @@ client.distube = new DisTube(client, {
 					'secure': false,
 					'session': false,
 					'storeId': '0',
-					'value': 'AKEyXzUrfXXQbfvrJ2FQI9u0e2_JXQk2fhU2oJ4C-DcCXKe9MoiMUqP70CnYNgtJXu5ahpp_aA',
+					'value': 'AKEyXzU86hQs-ewpdrDsG-pSZyKSsJxbpKZogBOVuEzvgWJKRDFmtw2rJXeoBntYkluPdk4gi0I',
 					'id': 16
 				},
 				{
 					'domain': '.youtube.com',
-					'expirationDate': 1766164960.941284,
+					'expirationDate': 1771800595.236452,
 					'hostOnly': false,
 					'httpOnly': true,
 					'name': 'SSID',
@@ -266,12 +266,12 @@ client.distube = new DisTube(client, {
 					'secure': true,
 					'session': false,
 					'storeId': '0',
-					'value': 'Aoq9Qt4P8_gl3vhye',
+					'value': 'AbNIB07gHHghlmwDd',
 					'id': 17
 				},
 				{
 					'domain': '.youtube.com',
-					'expirationDate': 1732442481,
+					'expirationDate': 1737372446,
 					'hostOnly': false,
 					'httpOnly': false,
 					'name': 'ST-l3hjtt',
@@ -280,26 +280,12 @@ client.distube = new DisTube(client, {
 					'secure': false,
 					'session': false,
 					'storeId': '0',
-					'value': 'session_logininfo=AFmmF2swRQIgPzHiN56q8eTcBAX-f4l0vEeMwBQ_iL14EqbA2iL-i6UCIQDkckN6fW07uATxg97QWl2DWLYjKRZ1JDLktBjJ9_VUHw%3AQUQ3MjNmeENMTUVVaFV6U2hkY09NdEdLSWJjM2pPanA4endjbFQ5SUhoTGRTT2NtTmJTZWFZVlRDRmJzalhRQVpOSlJKTTVrSFdPdGFjb21lSlRsR19rZFJsV0hjWGhYbVhwV01sSnlEbENRR013UUhkNkpVa0J5Vy1iUlEyQ3RNQTJIRTYxcENwdFdhM1ZDbDROcFJzejExZzVvREVxRFZ3',
+					'value': 'session_logininfo=AFmmF2swRAIgD5jJTXMljsgV85hG5JTsZs6tPpaRK7C11-hs5W7K89ICIFCFvGAl1RpyXflimmTKQReDdFGPVMx8ezeQmWbKuGtn%3AQUQ3MjNmeXVKeTI2MnROQ1Nxb2NaSlNzSjNKVXV4U1NvTnJLZklWdlBjZVExVnpTam5wb01jbWszQUg3cVJWZUZWQUd0Qm1uNzhDRVZXTnBRRDBhZ2JPUmltYlNZenAwR1pvSXlQWENqZ2gwbkRROWJGVTRuY2pmNnFPeEdpV3ZkbVVMalNqYVJqV3g1UjRta21VUnBjN0dFcXBSTkJaVGxR',
 					'id': 18
 				},
 				{
 					'domain': '.youtube.com',
-					'expirationDate': 1732442480,
-					'hostOnly': false,
-					'httpOnly': false,
-					'name': 'ST-tladcw',
-					'path': '/',
-					'sameSite': 'unspecified',
-					'secure': false,
-					'session': false,
-					'storeId': '0',
-					'value': 'session_logininfo=AFmmF2swRQIgPzHiN56q8eTcBAX-f4l0vEeMwBQ_iL14EqbA2iL-i6UCIQDkckN6fW07uATxg97QWl2DWLYjKRZ1JDLktBjJ9_VUHw%3AQUQ3MjNmeENMTUVVaFV6U2hkY09NdEdLSWJjM2pPanA4endjbFQ5SUhoTGRTT2NtTmJTZWFZVlRDRmJzalhRQVpOSlJKTTVrSFdPdGFjb21lSlRsR19rZFJsV0hjWGhYbVhwV01sSnlEbENRR013UUhkNkpVa0J5Vy1iUlEyQ3RNQTJIRTYxcENwdFdhM1ZDbDROcFJzejExZzVvREVxRFZ3',
-					'id': 19
-				},
-				{
-					'domain': '.youtube.com',
-					'expirationDate': 1732442481,
+					'expirationDate': 1737372448,
 					'hostOnly': false,
 					'httpOnly': false,
 					'name': 'ST-xuwub9',
@@ -308,12 +294,12 @@ client.distube = new DisTube(client, {
 					'secure': false,
 					'session': false,
 					'storeId': '0',
-					'value': 'session_logininfo=AFmmF2swRQIgPzHiN56q8eTcBAX-f4l0vEeMwBQ_iL14EqbA2iL-i6UCIQDkckN6fW07uATxg97QWl2DWLYjKRZ1JDLktBjJ9_VUHw%3AQUQ3MjNmeENMTUVVaFV6U2hkY09NdEdLSWJjM2pPanA4endjbFQ5SUhoTGRTT2NtTmJTZWFZVlRDRmJzalhRQVpOSlJKTTVrSFdPdGFjb21lSlRsR19rZFJsV0hjWGhYbVhwV01sSnlEbENRR013UUhkNkpVa0J5Vy1iUlEyQ3RNQTJIRTYxcENwdFdhM1ZDbDROcFJzejExZzVvREVxRFZ3',
-					'id': 20
+					'value': 'session_logininfo=AFmmF2swRAIgD5jJTXMljsgV85hG5JTsZs6tPpaRK7C11-hs5W7K89ICIFCFvGAl1RpyXflimmTKQReDdFGPVMx8ezeQmWbKuGtn%3AQUQ3MjNmeXVKeTI2MnROQ1Nxb2NaSlNzSjNKVXV4U1NvTnJLZklWdlBjZVExVnpTam5wb01jbWszQUg3cVJWZUZWQUd0Qm1uNzhDRVZXTnBRRDBhZ2JPUmltYlNZenAwR1pvSXlQWENqZ2gwbkRROWJGVTRuY2pmNnFPeEdpV3ZkbVVMalNqYVJqV3g1UjRta21VUnBjN0dFcXBSTkJaVGxR',
+					'id': 19
 				},
 				{
 					'domain': '.youtube.com',
-					'expirationDate': 1747992016.206658,
+					'expirationDate': 1752923150.458422,
 					'hostOnly': false,
 					'httpOnly': true,
 					'name': 'VISITOR_INFO1_LIVE',
@@ -322,12 +308,12 @@ client.distube = new DisTube(client, {
 					'secure': true,
 					'session': false,
 					'storeId': '0',
-					'value': '30K9zYdSJQY',
-					'id': 21
+					'value': 'vePaeqPbj4g',
+					'id': 20
 				},
 				{
 					'domain': '.youtube.com',
-					'expirationDate': 1747994478.468148,
+					'expirationDate': 1752924440.223592,
 					'hostOnly': false,
 					'httpOnly': true,
 					'name': 'VISITOR_PRIVACY_METADATA',
@@ -336,8 +322,8 @@ client.distube = new DisTube(client, {
 					'secure': true,
 					'session': false,
 					'storeId': '0',
-					'value': 'CgJQVBIcEhgSFhMLFBUWFwwYGRobHB0eHw4PIBAREiEgQA%3D%3D',
-					'id': 22
+					'value': 'CgJQVBIcEhgSFhMLFBUWFwwYGRobHB0eHw4PIBAREiEgNg%3D%3D',
+					'id': 21
 				},
 				{
 					'domain': '.youtube.com',
@@ -350,7 +336,7 @@ client.distube = new DisTube(client, {
 					'session': true,
 					'storeId': '0',
 					'value': '1',
-					'id': 23
+					'id': 22
 				},
 				{
 					'domain': '.youtube.com',
@@ -362,8 +348,8 @@ client.distube = new DisTube(client, {
 					'secure': true,
 					'session': true,
 					'storeId': '0',
-					'value': 'fPpd-WvzZ6s',
-					'id': 24
+					'value': 'Ax53SSch-RQ',
+					'id': 23
 				}
 			]
 		}),
@@ -386,6 +372,7 @@ client.distube
 	}))
 	.on('error', (e, queue, song) => {
 		queue.textChannel.send(`⛔ | Erro: ${e.toString().slice(0, 1974)}`);
+		console.log(e);
 	})
 	.on('empty', channel => channel.send({
 		embeds: [new EmbedBuilder().setColor('#FF0000').setDescription('⛔ | O canal está vazio! A sair...')]
